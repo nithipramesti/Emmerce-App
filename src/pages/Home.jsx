8f90089afeae427c81f54295348e15791f10cf38
nithipramesti <nithipramesti1508@gmail.com>
2021-07-15 20:22:09 +0800

    search & sort in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,22 @@ import { API_URL } from "../constant/API";
 class Home extends React.Component {
   state = {
     productList: [],
+    filteredProductList: [],
     page: 1,
     maxPage: 0,
     itemPerPage: 6,
+    searchProductName: "",
+    searchCategory: "",
+    sortBy: "",
   };
 
+  //take data from db.json and save it to the state, and set the max page
   fetchProducts = () => {
     Axios.get(`${API_URL}/products`)
       .then((result) => {
         this.setState({
           productList: result.data,
+          filteredProductList: result.data,
           maxPage: Math.ceil(result.data.length / this.state.itemPerPage),
         });
       })
@@ -24,12 +30,48 @@ class Home extends React.Component {
       });
   };
 
+  //displaying product per page
   renderProducts = () => {
     const beginningIndex = (this.state.page - 1) * this.state.itemPerPage;
-    const currentData = this.state.productList.slice(
+    let rawData = [...this.state.filteredProductList];
+
+    const compareString = (a, b) => {
+      if (a.productName < b.productName) {
+        return -1;
+      } else if (a.productName > b.productName) {
+        return 1;
+      } else {
+        return 0;
+      }
+    };
+
+    switch (this.state.sortBy) {
+      case "lowPrice":
+        rawData.sort((a, b) => a.price - b.price);
+        break;
+
+      case "highPrice":
+        rawData.sort((a, b) => b.price - a.price);
+        break;
+
+      case "az":
+        rawData.sort(compareString);
+        break;
+
+      case "za":
+        rawData.sort((a, b) => compareString(b, a));
+        break;
+
+      default:
+        rawData = [...this.state.filteredProductList];
+        break;
+    }
+
+    const currentData = rawData.slice(
       beginningIndex,
       beginningIndex + this.state.itemPerPage
     );
+
     return currentData.map((val) => {
       return <ProductCard productData={val} />;
     });
@@ -47,6 +89,32 @@ class Home extends React.Component {
     }
   };
 
+  inputHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({ [name]: value });
+  };
+
+  searchBtnHandler = () => {
+    const filteredProductList = this.state.productList.filter((val) => {
+      return (
+        val.productName
+          .toLowerCase()
+          .includes(this.state.searchProductName.toLowerCase()) &&
+        val.category
+          .toLowerCase()
+          .includes(this.state.searchCategory.toLowerCase())
+      );
+    });
+
+    this.setState({
+      filteredProductList,
+      maxPage: Math.ceil(filteredProductList.length / this.state.itemPerPage),
+      page: 1,
+    });
+  };
+
   componentDidMount() {
     this.fetchProducts();
   }
@@ -57,23 +125,34 @@ class Home extends React.Component {
         <div className="row">
           <div className="col-3">
             <div className="card">
-              <div className="card-reader">
+              <div className="card-header">
                 <strong>Filter Products</strong>
               </div>
               <div className="card-body">
                 <label htmlFor="searchProductName">Product Name</label>
                 <input
+                  onChange={this.inputHandler}
                   name="searchProductName"
                   type="text"
                   className="form-control mb-3"
                 />
                 <label htmlFor="searchCategory">Product Category</label>
-                <select name="searchCategory" className="form-control">
+                <select
+                  onChange={this.inputHandler}
+                  name="searchCategory"
+                  className="form-control"
+                >
                   <option value="">All Item</option>
-                  <option value="">Shirts</option>
-                  <option value="">Shoes</option>
-                  <option value="">Accessories</option>
+                  <option value="shirts">Shirts</option>
+                  <option value="pants">Pants</option>
+                  <option value="shoes">Shoes</option>
                 </select>
+                <button
+                  onClick={this.searchBtnHandler}
+                  className="btn btn-primary mt-3"
+                >
+                  Search
+                </button>
               </div>
             </div>
             <div className="card mt-4">
@@ -81,13 +160,17 @@ class Home extends React.Component {
                 <strong>Sort Product</strong>
               </div>
               <div className="card-body">
-                <label htmlFor="searchCategory">Sort by</label>
-                <select name="searchCategory" className="form-control">
+                <label htmlFor="sortBy">Sort by</label>
+                <select
+                  onChange={this.inputHandler}
+                  name="sortBy"
+                  className="form-control"
+                >
                   <option value="">Default</option>
-                  <option value="">Lowest Price</option>
-                  <option value="">Highest Price</option>
-                  <option value="">A-Z</option>
-                  <option value="">Z-A</option>
+                  <option value="lowPrice">Lowest Price</option>
+                  <option value="highPrice">Highest Price</option>
+                  <option value="az">A-Z</option>
+                  <option value="za">Z-A</option>
                 </select>
               </div>
             </div>
